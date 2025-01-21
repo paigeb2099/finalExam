1462b5e6e9d8a00111ac3eb5c35752973e5e2003
Paige Bonvallet <paige@retool.com>
2025-01-21 05:10:00 +0000

    test

--- apps/External apps/HuluNet Demo/lib/totalSpent.sql
+++ apps/External apps/HuluNet Demo/lib/totalSpent.sql
@@ -1,6 +1,16 @@
 SELECT
   type,
+  COUNT(*) AS COUNT,
   SUM(spend) AS total_budget_spent
-FROM hulunet_assets
-WHERE type IN ('Trailer', 'Social Media Post', 'Clip', 'Art', 'Billboard')
-GROUP BY type;
+FROM
+  hulunet_assets
+WHERE
+  type IN (
+    'Trailer',
+    'Social Media Post',
+    'Video Clip',
+    'Art',
+    'Billboard'
+  )
+GROUP BY
+  type;
